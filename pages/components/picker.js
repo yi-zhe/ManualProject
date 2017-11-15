ee08120997934093e31cf0950ff9f651e66a65c3
liuyang <liuyanggofurther@gmail.com>
2017-11-15 23:26:55 +0800

    components-picker-3

--- pages/components/picker.js
+++ pages/components/picker.js
@@ -16,7 +16,32 @@ Page({
     selectTime: '11:30',
     startDate: '2016-02-01',
     endDate: '2016-12-30',
-    selectDate: '2016-10-10'
+    selectDate: '2016-10-10',
+    list2: [{
+      date: '12月27日',
+      selected: true,
+      times: [{ time: '19:00' }, { time: '19:30' }, { time: '20:00' }, { time: '20:30' }, { time: '21:00' }]
+    }, {
+      date: '12月28日',
+      selected: false,
+      times: [{ time: '9:00' }, { time: '9:30' }, { time: '10:00' }, { time: '10:30' }, { time: '11:00' }]
+    }, {
+      date: '12月29日',
+      selected: false,
+      times: [{ time: '12:00' }, { time: '12:30' }, { time: '13:00' }, { time: '13:30' }, { time: '14:00' }]
+    }
+    ]
+  },
+
+  changeTime: function (e) {
+    var index = e.detail.value[0];
+    var list = this.data.list2;
+    var i, d;
+
+    for (i = 0; d = list[i]; ++i) {
+      d.selected = i == index ? true : false;
+    }
+    this.setData(this.data);
   },
 
   change1: function (e) {
